e8e167fa48a78a054a1a4ad5c69677761c4b5610
Erick <ericklopesalmeida8@gmail.com>
2023-08-16 21:53:56 -0300

    mudei o codigo

---
+++ index.js
@@ -0,0 +1,54 @@
+/*EXERCÍCIO 1 (ENUNCIADO ABAIXO)
+
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+(RESPOSTA)
+Vai aparecer no console o valor de "b" (10) 
+e em baixo vai aparecer o valor "a" do lado 
+do valor "b" alterado (10, 5). */
+
+
+
+/* EXERCÍCIO 2 (ENUNCIADO ABAIXO)
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+(RESPOSTA)
+No console vai aparecer ("b", "c" e "a") pois
+o a = b, b = c e c = a. */
+
+
+
+/* EXERCÍCIO 3 (ENUNCIADO ABAIXO)
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+(RESPOSTA)
+O "let p" poderia ser substituído por "let horasDia"
+e o "let t" por "let recebeDia". */
+
+
+
+/* console.log (typeof "nome", typeof "idade")
+// Acho que apareceu "string" pois são palavras e não números
+const nome = prompt ("Teu nome")
+const idade = prompt ("Idade?")
+console.log("Olá", nome, ", você tem", idade, "anos.") */
+
+const fome = prompt ("Ta com fome?")
+const frio = prompt ("Ta com frio?")
+const cansado = prompt ("Ta cansado?")
